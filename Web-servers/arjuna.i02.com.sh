075ab06856d713d6915eec4601bbf8cd993af3c0
mikhaelarya <5025211062@mhs.its.ac.id>
2023-10-11 10:50:06 +0700

    tidier script and fixing unzip - now with abs path

--- Web-servers/arjuna.i02.com.sh
+++ Web-servers/arjuna.i02.com.sh
@@ -13,7 +13,31 @@ echo "php php/timezone string Asia/Jakarta" | debconf-set-selections
 unset DEBIAN_FRONTEND
 
 
-curl -L -o "/var/www/arjuna.zip" "https://drive.google.com/uc?export=download&id=17tAM_XDKYWDvF-JJix1x7txvTBEax7vX" && unzip "/var/www/arjuna.zip" && rm "/var/www/arjuna.zip" && mv "/var/www/arjuna.yyy.com" "/var/www/arjuna.i02.com"
+
+echo '<VirtualHost *:80>
+    
+    ServerAdmin webmaster@localhost
+    DocumentRoot /var/www/arjuna.i02.com
+    ServerName arjuna.i02.com
+    ServerAlias www.arjuna.i02.com
+    
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+    
+</VirtualHost>' | tee /etc/apache2/sites-available/arjuna.i02.com.conf
+
+
+
+a2ensite arjuna.i02.com
+
+curl -L -o "/var/www/arjuna.zip" "https://drive.google.com/uc?export=download&id=17tAM_XDKYWDvF-JJix1x7txvTBEax7vX" && \
+sleep 1 && \
+unzip "/var/www/arjuna.zip" -d "/var/www/arjuna.yyy.com" && \
+sleep 1 && \
+mv "/var/www/arjuna.yyy.com" "/var/www/arjuna.i02.com" && \
+sleep 1 && \
+rm "/var/www/arjuna.zip"
+
 
 service apache2 start
 
