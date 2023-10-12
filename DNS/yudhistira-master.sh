11bb758d00f53f8c72affa87747a76b0504c5f48
mikhaelarya <5025211062@mhs.its.ac.id>
2023-10-12 19:24:04 +0700

    5 now truly done

--- DNS/yudhistira-master.sh
+++ DNS/yudhistira-master.sh
@@ -63,6 +63,26 @@ $TTL    604800
 2.229.192.in-addr.arpa.     IN      NS      i02.com.
 2                           IN      PTR     i02.com.' | tee /etc/bind/i02/2.229.192.in-addr.arpa
 
+echo 'zone "3.229.192.in-addr.arpa" {
+    type master;
+    file "/etc/bind/i02/3.229.192.in-addr.arpa";
+};' | tee -a /etc/bind/named.conf.local
+
+cp /etc/bind/db.local /etc/bind/i02/3.229.192.in-addr.arpa
+
+echo ';
+; BIND data file for local loopback interface
+;
+$TTL    604800
+@       IN      SOA     i02.com. root.i02.com. (
+                     2023101001         ; Serial
+                         604800         ; Refresh
+                          86400         ; Retry
+                        2419200         ; Expire
+                         604800 )       ; Negative Cache TTL
+;
+3.229.192.in-addr.arpa.     IN      NS      i02.com.
+4                           IN      PTR     abimanyu.i02.com.' | tee /etc/bind/i02/3.229.192.in-addr.arpa
 
 echo 'options {
         directory "/var/cache/bind";
